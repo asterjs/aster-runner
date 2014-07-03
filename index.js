e72bc18690859877d88dce846c4220ec0123adbc
Ingvar Stepanyan <me@rreverser.com>
2014-07-03 22:26:59 +0300

    Fixed for using with `dest`.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ module.exports = Rx.Observer.create(function onFiles(files) {
 	console.log('Processing files...');
 
 	files.subscribe(Rx.Observer.create(
-		function onFile(file) { console.log('>> %s'.yellow, file.loc.src) },
-		function onError(error) { console.error(notification.exception.stack.red) },
+		function onFile(file) { console.log('>> %s'.yellow, file.path) },
+		function onError(error) { console.error(error.stack.red) },
 		function onCompleted() { console.log('Done.'.green) }
 	));
 });
